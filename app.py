4c6ceb5e3494c0c26ccdca283db2724f7ae35b89
Nic <146491853+ic-nay@users.noreply.github.com>
2025-11-20 17:37:00 -0500

    Dummy stuff

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET', 'POST'])
+def default():
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method  == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            return process_image()
+    return "Error", 400
+
+def process_image():
+    return "TEST", 200
