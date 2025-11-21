6d0d86c5e03f6cfd2a0ac37e7efc1c8ff8efa2df
Nic <146491853+ic-nay@users.noreply.github.com>
2025-11-20 22:05:54 -0500

    Distinguishing between posts

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, redirect
 from video_manager import StreamWatcher
+import base64
 
 app = Flask(__name__)
 vid = StreamWatcher("rtsp://localhost:8554/live.sdp", 1)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def default():
-    return render_template("index.html")
+    if request.method == "POST":
+        if request.form.get("reconnect") != None:
+            return render_template("index.html", test="Stream Reconnect Request")
+        elif request.form.get("request") != None:
+            return  render_template("index.html", test="Frame Request")
+        else:
+            return "Error - Invalid Request", 400
+    return render_template("index.html", test="Normal Request")
+
+def get_image():
+    [result, image] = vid.capture_frame()
+    if result:
+        pass
+    else:
+        pass
--- video_manager.py
+++ video_manager.py
@@ -1,9 +1,4 @@
 import cv2
-import time
-import datetime
-import os
-
-cv2.utils.logging.setLogLevel(cv2.utils.logging.LOG_LEVEL_ERROR)
 
 class StreamWatcher():
     def __init__(self, stream_url, camera_id):
@@ -19,8 +14,6 @@ class StreamWatcher():
 
         print(f"📡 Connected to camera {self.camera_id}")
 
-        last_capture_time = time.time()
-
         ret, frame = self.cap.read()
 
         if not ret:
@@ -28,15 +21,3 @@ class StreamWatcher():
             return [False, "Failed to connect to camera {self.camera_id}"]
         
         return [True, frame]
-
-        current_time = time.time()
-        if current_time - last_capture_time >= FRAME_INTERVAL:
-            # Timestamp for filename
-            timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H-%M-%S")
-            filename = os.path.join(SAVE_PATH, f"{cameraid}{timestamp}.jpg")
-
-            # Save the frame
-            cv2.imwrite(filename, frame)
-            print(f"📸 Saved frame: {filename}")
-
-            last_capture_time = current_time
